31d4f32ffb7857acb04b95d03f5b2e3a35bad58a
Amartya Varma <amartya.varma@gmail.com>
2025-02-06 19:52:34 -0800

    f

---
+++ train.py
@@ -0,0 +1,32 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments
+import torch
+from datasets import load_dataset
+
+dsn = "amuvarma/humanml3d-flat-train-padded"
+model = AutoModelForCausalLM.from_pretrained(dsn)
+
+tokenizer = AutoTokenizer.from_pretrained(dsn)
+
+dataset = load_dataset(dsn, split="train")
+
+training_args = TrainingArguments(
+    overwrite_output_dir=True,
+    num_train_epochs=1,
+    per_device_train_batch_size=1,
+    logging_steps=1,
+    fp16=True,
+    # output_dir=f"./{base_repo_id}",
+    fsdp="auto_wrap",
+    report_to="wandb",
+    # save_steps=save_steps,
+    remove_unused_columns=True,
+    lr_scheduler_type="cosine"
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=dataset,
+)
+
+trainer.train()
