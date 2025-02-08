828f5b5faf59399e3391d2de4a1772358b2606e1
Amartya Varma <amartya.varma@gmail.com>
2025-02-07 17:57:44 -0800

    f

--- train.py
+++ train.py
@@ -30,6 +30,7 @@ training_args = TrainingArguments(
     fsdp="auto_wrap",
     report_to="wandb",
     save_steps=8374,
+    learning_rate=1e-4,
     
     # save_steps=save_steps,
     remove_unused_columns=True,
