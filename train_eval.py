83504d36317b65a5ffe68797f71eec47499f36a1
Amartya Varma <amartya.varma@gmail.com>
2025-02-08 15:16:33 -0800

    f

--- train_eval.py
+++ train_eval.py
@@ -2,30 +2,23 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingA
 from datasets import load_dataset
 import wandb
 
-# Define your dataset and model details
 dsn = "amuvarma/humanml3d-flat-train-padded-2"
+eval_dsn = "amuvarma/humanml3d-flat-train-padded-dedup-2"
 model_name = "meta-llama/Llama-3.2-1B"
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
-# Add extra tokens to the model's vocabulary
 num_add_tokens = 1000
 model.resize_token_embeddings(model.config.vocab_size + num_add_tokens)
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-# Load and shuffle your dataset
 dataset = load_dataset(dsn, split="train")
-dataset = dataset.shuffle(seed=42)
+train_dataset = dataset.shuffle(seed=42)
 
-# Split the dataset into training and evaluation sets (e.g., 90% train, 10% eval)
-split_dataset = dataset.train_test_split(test_size=0.1, seed=42)
-train_dataset = split_dataset["train"]
 eval_dataset = split_dataset["test"]
 
-# Initialize Weights & Biases for logging
 wandb.init(project="motiontrain-debug", name="4e2-1b")
 
-# Set up your training arguments
 training_args = TrainingArguments(
     output_dir="./output",
     overwrite_output_dir=True,
@@ -33,7 +26,7 @@ training_args = TrainingArguments(
     per_device_train_batch_size=16,
     logging_steps=10,
     evaluation_strategy="steps",  # Evaluate every `eval_steps` during training
-    eval_steps=20,               # Adjust this value as needed for your use case
+    eval_steps=1000,               # Adjust this value as needed for your use case
     bf16=True,
     learning_rate=2e-4,
     lr_scheduler_type="cosine",
@@ -42,7 +35,6 @@ training_args = TrainingArguments(
     remove_unused_columns=True
 )
 
-# Initialize the Trainer with both the training and evaluation datasets
 trainer = Trainer(
     model=model,
     args=training_args,
